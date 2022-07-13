3d54cbcdca94d5ad2ea64cc4c3131c36f91e334d
Oguma1028 <neetoguma@gmail.com>
2022-07-13 23:55:48 +0900

    inputコンポーネントにgridを適用

--- src/components/MessageInputField.js
+++ src/components/MessageInputField.js
@@ -1,15 +1,29 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import { Grid, Avatar } from "@material-ui/core";
 
 const useStyles = makeStyles({
   root: {
+    margin: "26px",
     gridRow: 2,
   },
 });
 
 const MessageInputField = () => {
   const styles = useStyles();
-  return <div className={styles.root}>インプット</div>;
+  return (
+    <>
+      <div className={styles.root}>
+        <Grid container>
+          <Grid xs={1}>
+            <Avatar />
+          </Grid>
+          <Grid xs={10}>入力</Grid>
+          <Grid xs={1}>ボタン</Grid>
+        </Grid>
+      </div>
+    </>
+  );
 };
 
 export default MessageInputField;
