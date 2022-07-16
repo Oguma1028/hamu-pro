8600ea2a7026669f246d55d85c6a39e2f7e25c07
Oguma1028 <neetoguma@gmail.com>
2022-07-16 22:56:47 +0900

    MUIのバージョンをv4に下げた

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,26 @@
 import React from "react";
+import MessageInputField from "./MessageInputField";
+import MessageList from "./MessageList";
+
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  root: {
+    display: "grid",
+    height: "100vh",
+    gridTemplateRows: "1fr auto",
+  },
+});
 
 const Main = ({ name }) => {
+  const classes = useStyles();
   console.log(name);
   return (
     <>
-      <p>メインです</p>
+      <div className={classes.root}>
+        <MessageList />
+        <MessageInputField name={name} />
+      </div>
     </>
   );
 };
